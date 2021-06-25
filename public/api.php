08900e2373c7d0054b479f598816456a1972360f
ShuangYa <i@sylingd.com>
2021-06-26 03:42:41 +0800

    remove dev mode

--- public/api.php
+++ public/api.php
@@ -9,9 +9,10 @@
  * @copyright Copyright (c) 2019 ShuangYa
  */
 error_reporting(E_ALL &~ E_NOTICE);
+define('IS_DEV', file_exists(__DIR__ . '/.dev'));
 define('PROJECT_PATH', realpath(__DIR__ . '/..') . '/');
 define('APP_PATH', PROJECT_PATH . 'app/');
-define('APP_ENV', 'develop');
+define('APP_ENV', IS_DEV ? 'develop' : 'product');
 define('PUBLIC_PATH', __DIR__ . '/');
 require(PROJECT_PATH . '/vendor/autoload.php');
 
