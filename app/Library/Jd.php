7de95be2704a29e8104d6f9c4d3c69b860a8db92
ShuangYa <i@sylingd.com>
2021-07-25 22:12:39 +0800

    chore: 使用移动API

--- app/Library/Jd.php
+++ app/Library/Jd.php
@@ -95,123 +95,106 @@ class Jd {
   }
 
   public static function getUrl($id) {
-    $searchData = [
-      'bonusIds' => null,
-      'categoryId' => null,
-      'cat2Id' => null,
-      'cat3Id' => null,
-      'deliveryType' => 0,
-      'fromCommissionRatio' => null,
-      'toCommissionRatio' => null,
-      'fromPrice' => null,
-      'hasCoupon' => 0,
-      'isHot' => null,
-      'preSale' => 0,
-      'isPinGou' => 0,
-      'jxFlag' => 0,
-      'isZY' => 0,
-      'isCare' => 0,
-      'lock' => 0,
-      'orientationFlag' => 0,
-      'sort' => null,
-      'sortName' => null,
-      'key' => $id,
-      'searchType' => 'st2',
-      'keywordType' => 'kt0',
-    ];
-
-    if (strpos($id, 'u.jd.com')) {
-      // 短链接
-      $searchData['searchType'] = 'st1';
-      $searchData['keywordType'] = 'kt1';
-    }
-
-    $search = Utils::fetchUrl('https://union.jd.com/api/goods/search', [
+    $search = Utils::fetchUrl('https://api.m.jd.com/?' . http_build_query([
+      'functionId' => 'unionSearch',
+      'client' => 'apple',
+      'clientVersion' => '3.9.2',
+      'appid' => 'u_jfapp',
+      'loginType' => 2,
+      't' => time(),
+      'body' => json_encode([
+        'funName' => 'search',
+        'version' => 'v2',
+        'source' => 30110,
+        'param' => [
+          'skuIds' => [$id]
+        ],
+        'isShowDetail' => 1,
+        'isNeedVideo' => 1,
+      ])
+    ]), [
       'json' => true,
       'cookie' => self::getCookie(),
       'header' => [
         'Content-type: application/json;charset=UTF-8',
-        'Referer: https://union.jd.com/'
-      ],
-      'post' => json_encode([
-        'pageNo' => 1,
-        'pageSize' => 1,
-        'searchUUID' => md5(uniqid()),
-        'data' => $searchData,
-      ])
+        'Origin: https://jingfenapp.jd.com',
+        'Referer: https://jingfenapp.jd.com/pages/search'
+      ]
     ]);
 
-    if ($search['code'] === 80001) {
+    if ($search['code'] !== 200) {
       return '系统错误，请联系管理员处理';
     }
 
     $good = null;
-    if (!isset($search['data']['unionGoods']) || count($search['data']['unionGoods']) === 0) {
-      return '未找到该商品，可能该商品不支持返利';
-    }
-    if (isset($search['data']['unionRecommendGoods']) && $search['data']['unionRecommendGoods'] === null) {
+    if (!isset($search['data']) || !isset($search['data']['skuPage']) || count($search['data']['skuPage']['result']) === 0) {
       return '未找到该商品，可能该商品不支持返利';
     }
 
     // 尝试拉一下GWD的API，看看有没有优惠券
-    $gwd = Utils::fetchUrl('https://m.gwdang.com/trend/data_new?' . http_build_query([
-      'opt' => 'product',
-      'dp_id' => $id . '-3',
-      'search_url' => 'https://item.jd.com/' . $id . '.html',
-      'period' => 180,
-      'from' => 'wx_1'
-    ]), [
-      'json' => true,
-      'timeout' => 3
-    ]);
-    if (is_array($gwd) && isset($gwd['data']) && isset($gwd['data']['dp_info']) && isset($gwd['data']['dp_info']['coupon'])) {
-      $url = $gwd['data']['dp_info']['coupon']['url'];
-      $discount = intval($gwd['data']['dp_info']['coupon']['amount']);
-      if (!isset($good['couponDiscount']) || $good['couponDiscount'] < $discount) {
-        parse_str(parse_url($url, PHP_URL_QUERY), $query);
-        $query = array_filter($query, function ($val) {
-          return strpos($val, 'https://coupon.m.jd.com') === 0;
-        });
-        if (count($query) > 0) {
-          $good['couponDiscount'] = $discount;
-          $good['couponLink'] = current($query);
-        }
-      }
-    }
+    // $gwd = Utils::fetchUrl('https://m.gwdang.com/trend/data_new?' . http_build_query([
+    //   'opt' => 'product',
+    //   'dp_id' => $id . '-3',
+    //   'search_url' => 'https://item.jd.com/' . $id . '.html',
+    //   'period' => 180,
+    //   'from' => 'wx_1'
+    // ]), [
+    //   'json' => true,
+    //   'timeout' => 3
+    // ]);
+    // if (is_array($gwd) && isset($gwd['data']) && isset($gwd['data']['dp_info']) && isset($gwd['data']['dp_info']['coupon'])) {
+    //   $url = $gwd['data']['dp_info']['coupon']['url'];
+    //   $discount = intval($gwd['data']['dp_info']['coupon']['amount']);
+    //   if (!isset($good['couponDiscount']) || $good['couponDiscount'] < $discount) {
+    //     parse_str(parse_url($url, PHP_URL_QUERY), $query);
+    //     $query = array_filter($query, function ($val) {
+    //       return strpos($val, 'https://coupon.m.jd.com') === 0;
+    //     });
+    //     if (count($query) > 0) {
+    //       $good['couponDiscount'] = $discount;
+    //       $good['couponLink'] = current($query);
+    //     }
+    //   }
+    // }
 
 
-    $good = $search['data']['unionGoods'][0][0];
-
-    $getCodeData = [
-      'couponLink' => '',
-      'isPinGou' => $good['isPinGou'],
-      'materialId' => $good['skuId'],
-      'materialType' => 1,
-      'planId' => $good['planId'],
-      'promotionId' => 3003764166,
-      'promotionType' => 3,
-      'promotionTypeId' => 4100447096,
-      'receiveType' => 'cps',
-      'wareUrl' => 'http://item.jd.com/' . $good['skuId'] . '.html',
-      'isSmartGraphics' => 0,
-      'requestId' => $good['requestId'],
-    ];
+    $good = $search['data']['skuPage']['result'][0];
     // 如果有优惠券的话
-    if (isset($good['couponLink'])) {
-      $getCodeData['couponLink'] = $good['couponLink'];
-    } else {
-      unset($getCodeData['couponLink']);
-    }
-    $getCode = Utils::fetchUrl('https://union.jd.com/api/receivecode/getCode', [
+    // if (isset($good['couponUrl'])) {
+    //   $getCodeData['couponUrl'] = $good['couponUrl'];
+    // } else {
+    //   unset($getCodeData['couponUrl']);
+    // }
+    $getCodeData = [
+      'shareType' => 1,
+      'skuId' => $id,
+      'requestId' => $good['requestId']
+    ];
+    $getCode = Utils::fetchUrl('https://api.m.jd.com/?' . http_build_query([
+      'functionId' => 'unionShare',
+      'client' => 'apple',
+      'clientVersion' => '3.9.2',
+      'appid' => 'u_jfapp',
+      'loginType' => 2,
+      't' => time(),
+      'body' => json_encode([
+        'funName' => 'share',
+        'source' => 30110,
+        'param' => [
+          'shareReq' => [
+            $getCodeData
+          ]
+        ],
+        'platform' => 2,
+      ])
+    ]), [
       'json' => true,
       'cookie' => self::getCookie(),
       'header' => [
         'Content-type: application/json;charset=UTF-8',
-        'Referer: https://union.jd.com/'
-      ],
-      'post' => json_encode([
-        'data' => $getCodeData
-      ])
+        'Origin: https://jingfenapp.jd.com',
+        'Referer: https://jingfenapp.jd.com/pages/search'
+      ]
     ]);
 
     $result = [
@@ -219,33 +202,21 @@ class Jd {
       'commission' => [
         [
           'type' => '普通会员',
-          'amount' => $good['wlCommission'],
-          'rate' => $good['wlCommissionRatio'],
+          'amount' => $good['commission'],
+          'rate' => $good['commissionShare'],
         ]
       ],
-      'qrcode' => $getCode['data']['data']['shortCode'],
-      'price' => $good['finalPrice'],
+      'qrcode' => $getCode['data']['shareInfo']['shortUrl'],
+      'price' => isset($good['couponAfterPrice']) ? $good['couponAfterPrice'] : $good['price'],
       'tag' => []
     ];
 
     if (isset($good['plusCommissionShare'])) {
       $result['commission'][] = [
         'type' => 'PLUS会员',
-        'amount' => round($good['finalPrice'] * $good['plusCommissionShare']) / 100,
+        'amount' => round($good['price'] * $good['plusCommissionShare']) / 100,
         'rate' => $good['plusCommissionShare']
       ];
-    }
-
-    if (isset($good['couponLink'])) {
-      $result['coupon'] = [
-        'total' => $good['couponQuota'],
-        'discount' => $good['couponDiscount'],
-        'qrcode' => ''
-      ];
-    }
-
-    if (isset($getCode['data']['data']['couponShortCode'])) {
-      $result['coupon']['qrcode'] = $getCode['data']['data']['couponShortCode'];
     }
 
     if ($good['isPinGou']) {
